e7426cba887c2bb4a5e82211fff7fbdba297ae9c
YigaoFan <fanyigao@qq.com>
2024-05-07 09:33:18 +0800

    section 2.9.2

--- src/MarkScanGC.hs
+++ src/MarkScanGC.hs
@@ -10,20 +10,26 @@ findDumpRoots :: TiDump -> [Addr]
 findDumpRoots dump = []
 
 findGlobalRoots :: TiGlobals -> [Addr]
-findGlobalRoots = foldl (\b (_, addr) -> addr : b) []
+findGlobalRoots = foldr (\(_, addr) b -> addr : b) []
 
-markFrom :: TiHeap -> Addr -> TiHeap
+markFrom :: TiHeap -> Addr -> (TiHeap, Addr)
 markFrom heap addr = do
   let node = heapLookup heap addr
   case node of
-    Marked _ -> heap
-    _        -> markInner node (heapUpdate heap addr (Marked node))
+    Marked _          -> (heap, addr)
+    IndirectNode a    -> markFrom heap a
+    _                 -> (markInner node (heapUpdate heap addr (Marked node)), addr)
   where
-    markInner (IndirectNode a) h = markFrom h a
-    markInner (Application a0 a1) h = markAddrs h [a0, a1]
-    markInner (Data _ addrs) h = markAddrs h addrs
+    markInner (Application a0 a1) h =
+      let (newH, newAddrs) = markAddrs h [a0, a1] in
+        heapUpdate newH addr (Marked (Application (head newAddrs) (newAddrs!!1)))
+    markInner (Data t addrs) h =
+      let (newH, newAddrs) = markAddrs h addrs
+       in heapUpdate newH addr (Marked (Data t newAddrs))
     markInner _ h = h
-    markAddrs = foldl markFrom
+    markAddrs h = foldr
+      (\a (prevH, addrs) -> let (newH, newA) = markFrom prevH a in (newH, newA : addrs))
+      (h, [])
 
 scan :: TiHeap -> TiHeap
 scan heap = do
@@ -32,7 +38,7 @@ scan heap = do
     case heapLookup h a of
       Marked node -> heapUpdate h a node
       _           -> heapFree h a
-    ) 
+    )
     heap addrs
 
 findRoots :: TiState -> [Addr]
@@ -43,7 +49,14 @@ findRoots (_, stack, dump, _, global, _) =
 
 gc :: TiState -> TiState
 gc state@(output, stack, dump, heap, global, stats) = do
-  let rs = findRoots state
-  let newHeap = foldl markFrom heap rs
-  (output, stack, dump, scan newHeap, global, stats)
+  let (heap1, newStack) = markFromStack heap stack
+  let (heap2, newDump) = markFromDump heap1 dump
+  let (heap3, newGlobals) = markFromGlobals heap2 global
+  (output, newStack, newDump, scan heap3, newGlobals, stats)
 
+markFromStack :: TiHeap -> TiStack -> (TiHeap, TiStack)
+markFromStack heap = foldr (\a (h, newStack) -> let (newH, newA) = markFrom h a in (newH, newA : newStack)) (heap, [])
+markFromDump :: TiHeap -> TiDump -> (TiHeap, TiDump)
+markFromDump heap dump = (heap, dump)
+markFromGlobals :: TiHeap -> TiGlobals -> (TiHeap, TiGlobals)
+markFromGlobals heap = foldr (\(name, a) (h, newGlobals) -> let (newH, newA) = markFrom h a in (newH, (name, newA) : newGlobals)) (heap, [])
