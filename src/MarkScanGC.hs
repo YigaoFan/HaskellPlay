22575bc7ca535e0d2720aaf8998a57137ecfd14a
YigaoFan <fanyigao@qq.com>
2024-05-14 09:16:02 +0800

    Section 2.9.3

--- src/MarkScanGC.hs
+++ src/MarkScanGC.hs
@@ -3,6 +3,7 @@ import Heap (Addr, heapUpdate, heapLookup, addrsOf, heapFree)
 import Compiler (MarkState (Done, Visits), Node (Application, Data, IndirectNode, Marked, Prim, SuperCombinator, Num, String), TiDump, TiGlobals, TiHeap, TiStack, TiState)
 import Debug.Trace (trace)
 import Data.Maybe (Maybe)
+import GHC.Stack (HasCallStack)
 
 findStackRoots :: TiStack -> [Addr]
 findStackRoots stack = stack
@@ -13,71 +14,54 @@ findDumpRoots dump = []
 findGlobalRoots :: TiGlobals -> [Addr]
 findGlobalRoots = foldr (\(_, addr) b -> addr : b) []
 
-isMarkDone heap addr =
-  case heapLookup heap addr of
-    Marked Done n -> True
-    _             -> False
-isApplication heap addr =
-  case heapLookup heap addr of
-    Application _ _ -> True
-    _               -> False
-isPrim heap addr =
-  case heapLookup heap addr of
-    Prim _ _ -> True
-    _        -> False
-
 isFinal :: (Addr, Maybe Addr, TiHeap) -> Node -> Maybe Node -> Bool
-isFinal (_, Nothing, heap) (Marked Done _) _ = True
+isFinal (_, Nothing, heap) (Marked Done _) Nothing = True
 isFinal _ _ _ = False
 -- So good to add these two args to let them can be pattern match
-
--- 需要为这里专门搞个 Application 类型来兼容不同的 a1 a2 类型
+-- 需要为这里专门搞个 Application 类型来兼容不同的 a1 a2 类型，后来思考下程序的语义给 Visits 里加了两项
+-- 这里面涉及的地址都有可能要更新啊，不能直接用原来的 node
 mark :: (Addr, Maybe Addr, TiHeap) -> Node -> Maybe Node -> (Addr, Maybe Addr, TiHeap)
 mark (_, backward, heap) (IndirectNode a) _ = (a, backward, heap)
+
 mark (f, backward, heap) node@(Application a1 a2) _ =
   (a1, Just f, heapUpdate heap f (Marked (Visits 1 [] backward) node))
-mark (f, backward@(Just b), heap) (Marked Done _) (Just (Marked (Visits 1 [] backward') node@(Application _ a2)))
-  = (a2, backward, heapUpdate heap b (Marked (Visits 2 [f] backward') node))
-mark (f, Just b, heap) (Marked Done _) (Just (Marked (Visits 2 [a1] backward') node@(Application {}))) -- 检查 backward' 的正确性
-  = (b, backward', heapUpdate heap b (Marked Done (Application a1 f))) -- think about [Addr] here
--- 不确定这个b'是不是有效地址
-mark (f, backward, heap) node _ =            (f, backward, heapUpdate heap f (Marked Done node))
+mark (f, backward@(Just b), heap) (Marked Done _) (Just (Marked (Visits 1 [] backward') node@(Application _ a2))) =
+  (a2, backward, heapUpdate heap b (Marked (Visits 2 [f] backward') node))
+mark (f, Just b, heap) (Marked Done _) (Just (Marked (Visits 2 [a1] backward') node@(Application {}))) =
+  (b, backward', heapUpdate heap b (Marked Done (Application a1 f)))
 
--- TODO change
+mark (f, backward, heap) node@(Data _ []) _ =
+  (f, backward, heapUpdate heap f (Marked Done node))
+mark (f, backward, heap) node@(Data _ (a1 : addrs)) _ =
+  (a1, Just f, heapUpdate heap f (Marked (Visits 1 [] backward) node))
+mark (f, backward@(Just b), heap) (Marked Done _) (Just (Marked (Visits n visits backward') node@(Data t coms))) =
+  let newVisits = f : visits in
+    if n == length coms
+      then (b, backward', heapUpdate heap b (Marked Done (Data t (reverse newVisits))))
+      else (coms!!n, backward, heapUpdate heap b (Marked (Visits (n+1) newVisits backward') node))
+
+mark (f, backward, heap) node@(SuperCombinator {}) _ = (f, backward, heapUpdate heap f (Marked Done node))
+mark (f, backward, heap) node@(Num {}) _ = (f, backward, heapUpdate heap f (Marked Done node))
+mark (f, backward, heap) node@(String {}) _ = (f, backward, heapUpdate heap f (Marked Done node))
+mark (f, backward, heap) node@(Prim {}) _ = (f, backward, heapUpdate heap f (Marked Done node))
+mark _ fwdNode bwdNode = error ("no match function mark" ++ show fwdNode ++ show bwdNode)
+
 markFrom :: TiHeap -> Addr -> (TiHeap, Addr)
 markFrom heap addr = do
-  let (f, _, h) = iter (addr, Nothing, heap) (heapLookup heap addr) Nothing in
-    (h, f) -- 之前那个返回的地址怎么处理
+  let (f, _, h) = iter (addr, Nothing, heap) in
+    (h, f)
   where
-    iter :: (Addr, Maybe Addr, TiHeap) -> Node -> Maybe Node -> (Addr, Maybe Addr, TiHeap)
-    iter state@(f, b, h) fwdNode bwdNode = 
-      if isFinal state fwdNode bwdNode
-        then state -- 之前那个返回的地址怎么处理
-        else let s@(f', b', h') = mark state fwdNode bwdNode in
-          iter s (heapLookup h' f') 
-          (case b' of
-            Just a -> Just (heapLookup h' a)
+    iter state@(f, b, h) =
+      let
+        fwdNode = heapLookup h f
+        bwdNode =
+          (case b of
+            Just a -> Just (heapLookup h a)
             _ -> Nothing)
-
-    -- then (h, addr) --addr here not correct
-    -- else markFrom h addr
-  -- let newS@(_, _, h) = uncurry mark s
-  -- let node = heapLookup heap addr
-  -- case node of
-  --   Marked _          -> (heap, addr)
-  --   IndirectNode a    -> markFrom heap a
-  --   _                 -> (markInner node (heapUpdate heap addr (Marked node)), addr)
-  -- where
-  --   markInner (Application a0 a1) h =
-  --     let (newH, newAddrs) = markAddrs h [a0, a1] in
-  --       heapUpdate newH addr (Marked (Application (head newAddrs) (newAddrs!!1)))
-  --   markInner (Data t addrs) h =
-  --     let (newH, newAddrs) = markAddrs h addrs
-  --      in heapUpdate newH addr (Marked (Data t newAddrs))
-  --   markInner _ h = h
-  --   markAddrs h = foldr
-  --     (\a (prevH, addrs) -> let (newH, newA) = markFrom prevH a in (newH, newA : addrs))
-  --     (h, [])
+        in
+        if isFinal state fwdNode bwdNode
+          then state -- 之前那个返回的地址怎么处理：用 final state 下的 f，观察下来此时 f 就对应最初的那个节点的 Marked Done
+          else iter $ mark state fwdNode bwdNode
 
 scan :: TiHeap -> TiHeap
 scan heap = do
